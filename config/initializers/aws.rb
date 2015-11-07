d51d5784563d2a96e9bcdc0c5b5060939955a6c4
Chip Self <chipself9@gmail.com>
2015-11-06 19:33:35 -0500

    hid s3 creds

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,8 +1,6 @@
 Aws.config.update({
  region: 'us-east-1',
- credentials: Aws::Credentials.new('AKIAJNGMMW3LLR24NJ2A', '8JI0r1UTQ41Vg02EZMPyIbsF0YxKRv2tpUg7iXFj')
-})
+ credentials: Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY']),})
 
-#Aws.config(access_key_id: 'AKIAJNGMMW3LLR24NJ2A', secret_access_key: '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', region: 'us-west-2')
 
-S3_BUCKET = Aws::S3::Resource.new.bucket('bbr-development')
+S3_BUCKET = Aws::S3::Resource.new.bucket(ENV['S3_BUCKET'])
